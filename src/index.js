b0849d9055c0b4f61daa1f39d6ba21af552fdb68
Georjane <pzgeorjane@gmail.com>
2021-01-13 11:07:45 +0100

    Set up asset management for css and images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import './style.css';
+import Rose from './pic.jpg'
 alert('hello world');
+
+const body = document.querySelector('body');
+// const element = document.createElement('div');
+const myRose = new Image();
+myRose.src = Rose;
+
+body.appendChild(myRose);
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,17 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
   mode: 'development'
 };
