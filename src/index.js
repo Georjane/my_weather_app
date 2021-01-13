42415e22f49702683c086edfebf63764481b71f3
Georjane <pzgeorjane@gmail.com>
2021-01-13 17:58:30 +0100

    Adjust footer

--- src/index.js
+++ src/index.js
@@ -30,36 +30,40 @@ getCityBtn.addEventListener("click",function(e){
 });
 
 async function getWeatherData() {
-  let cityName = cityNameInput.value
-  cityName !== '' ? cityName : console.log('error')
+  try {
+    let cityName = cityNameInput.value
+    // cityName !== '' ? cityName : console.log('error')
 
-  let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=9e451d75ffc63a816314ca34a0f0ff4e';
-  const response = await fetch(url, {mode: 'cors'});
-  const cityData = await response.json();
-  const city = await cityData;
-  console.log(city);
-  cityTag.innerHTML = city.name + ', ' + city.sys.country;
-  cityTemp.innerHTML = kelvinToCelsius(city.main.temp);
-  date.innerHTML = Date().slice(16, 21) + ', ' + Date().slice(0, 10);
-  cityNameInput.value = '';
-  wind.innerHTML = 'Wind speed: ' + city.wind.speed + 'm/s'
-  visibility.innerHTML = 'Visibility: ' + (city.visibility)/1000 + 'km'
-  humidity.innerHTML = 'Humidity: ' + city.main.humidity + '%'
-
-  tempCBtn.addEventListener('click', function(e) {
-    e.preventDefault();
+    let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=9e451d75ffc63a816314ca34a0f0ff4e';
+    const response = await fetch(url, {mode: 'cors'});
+    const cityData = await response.json();
+    const city = await cityData;
+    cityTag.innerHTML = city.name + ', ' + city.sys.country;
     cityTemp.innerHTML = kelvinToCelsius(city.main.temp);
-  });
-  tempFBtn.addEventListener('click', function(e) {
-    e.preventDefault();
-    cityTemp.innerHTML = kelvinToFarenheit(city.main.temp);
-  })
-  
-  if (city.main.temp >= 293) {
-    tempDescription.innerHTML = 'It\' a Sunny day';
-  } else {
-    tempDescription.innerHTML = 'It\' a Cold day';
-  }
+    date.innerHTML = Date().slice(16, 21) + ', ' + Date().slice(0, 10);
+    cityNameInput.value = '';
+    wind.innerHTML = 'Wind speed: ' + city.wind.speed + 'm/s'
+    visibility.innerHTML = 'Visibility: ' + (city.visibility)/1000 + 'km'
+    humidity.innerHTML = 'Humidity: ' + city.main.humidity + '%'
+
+    tempCBtn.addEventListener('click', function(e) {
+      e.preventDefault();
+      cityTemp.innerHTML = kelvinToCelsius(city.main.temp);
+    });
+    tempFBtn.addEventListener('click', function(e) {
+      e.preventDefault();
+      cityTemp.innerHTML = kelvinToFarenheit(city.main.temp);
+    })
+    
+    if (city.main.temp >= 293) {
+      tempDescription.innerHTML = 'It\' a Sunny day';
+    } else {
+      tempDescription.innerHTML = 'It\' a Cold day';
+    }
+  } catch (error) {
+    alert('Invalid City name: City not found!');
+    cityNameInput.value = '';
+  };
 }
 
 function kelvinToCelsius(temp) {
